206cd19773c401c64d584f6b7ae4f8f6364d8776
Jørund Amsen <jorund.amsen@bekk.no>
2018-05-23 10:17:06 +0200

    Show player hp on the playscreen

--- src/main/java/screens/PlayScreen.java
+++ src/main/java/screens/PlayScreen.java
@@ -30,6 +30,9 @@ public class PlayScreen implements Screen {
 
         displayTiles(terminal, left, top);
         displayCreatures(terminal, left, top);
+
+        String stats = String.format(" %3d/%3d hp", player.hp(), player.maxHp());
+        terminal.write(stats, 1, 23);
     }
 
     public Screen respondToUserInput(KeyEvent key) {
